5d892702068ea2f43c86a441721c07d0e7ae970d
Oscar Torres <oscar@oscartorres.co>
2026-05-04 00:33:16 -0500

    fix(api): correct bff API path to the appropriate URL

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@ const API_PATHS = {
   product: "https://lhw5zk38tc.execute-api.us-east-2.amazonaws.com/prod",
   order: "https://q5jquu68j1.execute-api.us-east-2.amazonaws.com/prod",
   import: "https://vurlfxib20.execute-api.us-east-2.amazonaws.com/prod",
-  bff: "https://q5jquu68j1.execute-api.us-east-2.amazonaws.com/prod",
+  bff: "https://lhw5zk38tc.execute-api.us-east-2.amazonaws.com/prod",
   cart: "https://q5jquu68j1.execute-api.us-east-2.amazonaws.com/prod",
 };
 
