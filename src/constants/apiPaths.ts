9a7b1e4f294dc4c0b417d34303bc37d57b76519d
Oscar Torres <oscar@oscartorres.co>
2026-05-03 23:29:54 -0500

    fix(api): update product and import API paths to correct URLs

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
-  product: "https://q5jquu68j1.execute-api.us-east-2.amazonaws.com/prod",
+  product: "https://lhw5zk38tc.execute-api.us-east-2.amazonaws.com/prod",
   order: "https://q5jquu68j1.execute-api.us-east-2.amazonaws.com/prod",
-  import: "https://q5jquu68j1.execute-api.us-east-2.amazonaws.com/prod",
+  import: "https://vurlfxib20.execute-api.us-east-2.amazonaws.com/prod",
   bff: "https://q5jquu68j1.execute-api.us-east-2.amazonaws.com/prod",
   cart: "https://q5jquu68j1.execute-api.us-east-2.amazonaws.com/prod",
 };
